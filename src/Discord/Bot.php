cfe6bfd3267374e8a6d495e224d409239c613d18
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-20 11:01:44 +0700

    Update d

--- src/Discord/Bot.php
+++ src/Discord/Bot.php
@@ -45,6 +45,7 @@ final class Bot
 				$guild_id = $message->channel->guild_id;
 				$channel_id = $message->channel_id;
 				$guild = $this->discord->guilds->get("id", $guild_id);
+				var_dump($guild);
 				$channel = $guild->channels->get("id", $channel_id);
 
 

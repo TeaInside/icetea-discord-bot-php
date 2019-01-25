543d14f5e83f2fa19168c95f925cba3740c06224
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-25 23:33:15 +0700

    Update discrod

--- src/DiscordDaemon/Bot.php
+++ src/DiscordDaemon/Bot.php
@@ -138,7 +138,7 @@ final class Bot
 					$a = (new Response($discord, $message))->run();
 					var_dump($a);	
 				} catch (\Error $e) {
-					var_dump($e->getMessage());
+					var_dump($e);
 				}
 				// $pool->submit(new Response($discord, $message));
 			});

be2d2699553ef487708332b2808731bf75bb16cf
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-27 13:35:04 +0700

    Add debug flag

--- src/DiscordDaemon/StreamQueue.php
+++ src/DiscordDaemon/StreamQueue.php
@@ -107,60 +107,8 @@ class StreamQueue
 								$guild = $discord->guilds->get("id", $guild_id);
 								$channel = $guild->channels->getAll("type", "text")->first();
 								$channel->sendMessage($r)->then(function ($message) use (&$file) {
-								    printf("The message was sent ~!\n");
-								    var_dump($file);
-								    if (is_string($file)) {
-								    	var_dump($file);
-								    	$file = STORAGE_PATH."/mp3/{$file}";
-								    	var_dump($file);
-								    	var_dump("wmm");
-								    	sleep(100);
-								   //  	if (!pcntl_fork()) {
-								   //  		printf("[StreamQueue] initd\n");
-								   //  		$this->bot->discord->init();
-								   //  		$this->bot->discord->on("ready", function ($discord) use (&$guild_id, &$channel_id, &$file) {
-											// 	printf("[StreamQueue] Streaming is ready!\n");
-											// 	$guild = $discord->guilds->get("id", $guild_id);
-											// 	$channel = $guild->channels->getAll("type", "voice")->first();
-											// 	$discord->joinVoiceChannel($voiceChannel, false, false, null)->then(
-											// 		function (VoiceClient $vc) use (&$file) {
-											// 		    printf("[StreamQueue] Joined voice channel...\n");
-											// 		    printf("[StreamQueue] Playing %s...\n", $file);
-											// 		    $vc->setBitrate(128000)->then(
-											// 	    		function () use ($vc, &$file) {
-											// 		    		$vc->playFile($file)->otherwise(function($e){ 
-											// 		    			printf("Error: %s\n", $e->getMessage());
-											// 		    			exit;
-											// 		    		})->then(function () {
-											// 		    			exit;
-											// 		    		});
-											// 	    		}
-											// 	    	)->otherwise(function($e){ 
-											// 	    		printf("Error: %s\n", $e->getMessage());
-											// 	    		exit;
-											// 	    	});
-											// 		},
-
-											// 		function ($e) {
-											// 	    	printf(
-											// 	    		"There was an error joining the voice channel: %s\n",
-											// 	    		$e->getMessage()
-											// 	    	); 
-											// 		}
-											// 	)->otherwise(
-											// 		function ($e) {
-											// 	    	printf(
-											// 	    		"There was an error joining the voice channel: %s\n",
-											// 	    		$e->getMessage()
-											// 	    	); 
-											// 		}
-											// 	);
-											// });
-											// $this->bot->discord->run();
-								   //  		exit;
-								   //  	}
-								   //  	pcntl_wait($status);
-								    }
+									var_dump("memset");
+								    // printf("The message was sent ~!\n");
 								    exit;
 								})->otherwise(function ($e) {
 								    printf("There was an error sending the message: %s\n", $e->getMessage());
@@ -170,59 +118,6 @@ class StreamQueue
 							$this->bot->discord->run();
 							exit;
 					    }
-					    var_dump("waiting...");
-					    pcntl_wait($status);
-					    $status = null;
-					    var_dump($file);
-					   //  if (is_string($file)) {
-					   //  	$file = STORAGE_PATH."/mp3/{$file}";
-					   //  	if (!pcntl_fork()) {
-					   //  		printf("[StreamQueue] initd\n");
-					   //  		$this->bot->discord->init();
-					   //  		$this->bot->discord->on("ready", function ($discord) use (&$guild_id, &$channel_id, &$file) {
-								// 	printf("[StreamQueue] Streaming is ready!\n");
-								// 	$guild = $discord->guilds->get("id", $guild_id);
-								// 	$channel = $guild->channels->getAll("type", "voice")->first();
-								// 	$discord->joinVoiceChannel($voiceChannel, false, false, null)->then(
-								// 		function (VoiceClient $vc) use (&$file) {
-								// 		    printf("[StreamQueue] Joined voice channel...\n");
-								// 		    printf("[StreamQueue] Playing %s...\n", $file);
-								// 		    $vc->setBitrate(128000)->then(
-								// 	    		function () use ($vc, &$file) {
-								// 		    		$vc->playFile($file)->otherwise(function($e){ 
-								// 		    			printf("Error: %s\n", $e->getMessage());
-								// 		    			exit;
-								// 		    		})->then(function () {
-								// 		    			exit;
-								// 		    		});
-								// 	    		}
-								// 	    	)->otherwise(function($e){ 
-								// 	    		printf("Error: %s\n", $e->getMessage());
-								// 	    		exit;
-								// 	    	});
-								// 		},
-
-								// 		function ($e) {
-								// 	    	printf(
-								// 	    		"There was an error joining the voice channel: %s\n",
-								// 	    		$e->getMessage()
-								// 	    	); 
-								// 		}
-								// 	)->otherwise(
-								// 		function ($e) {
-								// 	    	printf(
-								// 	    		"There was an error joining the voice channel: %s\n",
-								// 	    		$e->getMessage()
-								// 	    	); 
-								// 		}
-								// 	);
-								// });
-								// $this->bot->discord->run();
-					   //  		exit;
-					   //  	}
-					   //  	pcntl_wait($status);
-					   //  }
-					    exit;
 					};
 
 					$channel->sendMessage($r)->then(function ($message) use ($act, $channel, $notify) {
